dedf22279ec348c970fcece5f4dea334b7c60439
micheci <michecimartinez@gmail.com>
2024-04-15 23:02:05 -0500

    started work on product Details page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
+import ProductDetails from "./Pages/ProductDetails";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="home" element={<Home />} />
           <Route path="profile" element={<div>Not Found</div>} />
+          <Route path="product/:id" element={<ProductDetails />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/Components/Product.tsx
+++ src/Components/Product.tsx
@@ -1,4 +1,6 @@
 import { Box, Rating, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
 interface itemDataProps {
   item_id: number;
   item_name: string;
@@ -7,37 +9,39 @@ interface itemDataProps {
 
 const Product = (itemData: itemDataProps) => {
   return (
-    <Box
-      sx={{
-        height: "300px",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <Link to={`/product/${itemData.item_id}`}>
       <Box
-        component="img"
         sx={{
-          width: "100%",
-          height: "200px",
-          objectFit: "cover",
+          height: "300px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
         }}
-        alt="The house from the offer."
-        src={itemData.pictureurl}
-      />
-      <Typography
-        sx={{ alignItems: "center", textAlign: "center" }}
-        variant="h6"
       >
-        {itemData.item_name}
-      </Typography>
+        <Box
+          component="img"
+          sx={{
+            width: "100%",
+            height: "200px",
+            objectFit: "cover",
+          }}
+          alt="The house from the offer."
+          src={itemData.pictureurl}
+        />
+        <Typography
+          sx={{ alignItems: "center", textAlign: "center" }}
+          variant="h6"
+        >
+          {itemData.item_name}
+        </Typography>
 
-      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-        <Rating name="disabled" value={4} disabled />
-        <Typography variant="h6">$99.99{itemData.item_id}</Typography>
+        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+          <Rating name="disabled" value={4} disabled />
+          <Typography variant="h6">$99.99{itemData.item_id}</Typography>
+        </Box>
       </Box>
-    </Box>
+    </Link>
   );
 };
 
