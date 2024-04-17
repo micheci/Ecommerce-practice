b7a9c9f284229d3872a142d4db01de29a9670dc7
micheci <michecimartinez@gmail.com>
2024-04-16 22:27:52 -0500

    wip product details page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import ProductDetails from "./Pages/ProductDetails";
+import ProductMenu from "./Components/ProductMenu";
+import { useState } from "react";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const handleCategorySelect = (category) => {
+    console.log("Selected category:", category);
+    setSelectedCategory(category);
+  };
+
   return (
     <>
       <BrowserRouter>
+        <ProductMenu onCategorySelect={handleCategorySelect} />
         <Routes>
           <Route index element={<Home />} />
           <Route path="home" element={<Home />} />
--- src/Pages/ProductDetails.tsx
+++ src/Pages/ProductDetails.tsx
@@ -1,7 +1,152 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { blueGrey, red, lightGreen, blue } from "@mui/material/colors";
+
+import {
+  Box,
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Grid,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  Typography,
+} from "@mui/material";
+
+interface Product {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+  size: string;
+  color: string;
+}
+
+// This is a placeholder for the actual product data
+const product: Product = {
+  id: 1,
+  name: "Product Name",
+  image: "https://via.placeholder.com/150",
+  price: 99.99,
+  size: "M",
+  color: "Blue",
+};
 
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  const { id } = useParams<{ id: string }>();
+
+  // Fetch product data based on `id` and display it
+
+  return (
+    <Box marginX="auto" marginTop={4} maxWidth="lg">
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <img
+            src={product.image}
+            alt={product.name}
+            style={{ width: "100%" }}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h4">{product.name}</Typography>
+          <Typography variant="h5">${product.price}</Typography>
+          <Typography variant="body1">
+            Size:
+            <Select
+              value={product.size}
+              onChange={(event) => {
+                /* handle size change */
+              }}
+            >
+              <MenuItem value="small">Small</MenuItem>
+              <MenuItem value="medium">Medium</MenuItem>
+              <MenuItem value="large">Large</MenuItem>
+              <MenuItem value="x-large">X-Large</MenuItem>
+            </Select>
+          </Typography>{" "}
+          {/* Color Picker */}
+          <FormControl>
+            <FormLabel id="demo-radio-buttons-group-label">Color</FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="female"
+              name="radio-buttons-group"
+              row
+            >
+              <FormControlLabel
+                value="red"
+                control={
+                  <Radio
+                    sx={{
+                      color: red[800],
+                      "&.Mui-checked": {
+                        color: red[600],
+                      },
+                    }}
+                  />
+                }
+                label="red"
+              />
+              <FormControlLabel
+                value="black"
+                control={
+                  <Radio
+                    sx={{
+                      color: blueGrey[800],
+                      "&.Mui-checked": {
+                        color: blueGrey[600],
+                      },
+                    }}
+                  />
+                }
+                label="black"
+              />
+              <FormControlLabel
+                value="blue"
+                control={
+                  <Radio
+                    sx={{
+                      color: blue[800],
+                      "&.Mui-checked": {
+                        color: blue[600],
+                      },
+                    }}
+                  />
+                }
+                label="blue"
+              />
+              <FormControlLabel
+                value="sageGreen"
+                control={
+                  <Radio
+                    sx={{
+                      color: lightGreen[800],
+                      "&.Mui-checked": {
+                        color: lightGreen[600],
+                      },
+                    }}
+                  />
+                }
+                label="Sage Green"
+              />
+            </RadioGroup>
+          </FormControl>{" "}
+          <Box mt={2}>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ width: "100%" }}
+            >
+              Add to Cart
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default ProductDetails;
