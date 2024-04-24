8bf163a840544802778cddff4e49e63abd71a464
micheci <michecimartinez@gmail.com>
2024-04-23 22:08:50 -0500

    added login/signup and jwt authorization

---
+++ backend/routes/auth.js
@@ -0,0 +1,85 @@
+import express from "express";
+import pool from "../db.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import process from "process";
+
+const router = express.Router();
+
+// Sign up
+router.post("/signup", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    //Check if username already
+    const userNameResult = await pool.query(
+      "SELECT * FROM users WHERE username = $1",
+      [username]
+    );
+    if (userNameResult.rowCount) {
+      res.json({ status: false, message: "Username already exists" });
+      return;
+    }
+
+    // Time to hash the password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    // Inserting into the database
+    await pool.query(
+      "INSERT INTO users (username, password,created_at) VALUES ($1, $2,NOW())",
+      [username, hashedPassword]
+    );
+
+    res.json({ status: true, message: "Signup successful" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const userNameResult = await pool.query(
+      "SELECT * FROM users WHERE username = $1",
+      [username]
+    );
+    if (!userNameResult.rowCount) {
+      res.json({ status: false, message: "User not found" });
+      return;
+    }
+    if (await bcrypt.compare(password, userNameResult.rows[0].password)) {
+      const token = jwt.sign(
+        { id: userNameResult.id },
+        process.env.SECRET_KEY,
+        {
+          expiresIn: "1h",
+        }
+      );
+
+      res.json({ status: true, message: "Login successful", token: token });
+    } else {
+      res.json({ status: false, message: "Incorrect password" });
+      res.json({ status: false, message: "Incorrect password" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Middleware to authenticate token
+export const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.sendStatus(401); // if there isn't any token
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next(); // pass the execution off to whatever request the client intended
+  });
+};
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,37 @@
 // Import required modules
-import express, { json } from 'express';
-import cors from 'cors';
+import express, { json } from "express";
+import cors from "cors";
 import session from "express-session";
-import passport from "passport"
-import router from './routes/routes.js';
+import dotenv from "dotenv";
+import router from "./routes/routes.js";
+import auth from "./routes/auth.js";
+import passport from "passport";
 
 // Create an Express application
 const app = express();
 const port = 3000;
-
+dotenv.config();
+////////////////////////////////// Middleware //////////////////////////////////
 // Middleware to parse JSON requests
 app.use(json());
 // Enable CORS for all routes
 app.use(cors());
-app.use(session({
-  secret: "bellaco",
-  saveUninitialized:false,
-  resave:false,
-  cookie:{
-    maxAge: 60*60*1000
-  }
-}))
-
+app.use(
+  session({
+    secret: "bellaco",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 60 * 60 * 1000,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+/////////////////////////////////////////////////
 // Use the router
-app.use('/', router);
+app.use("/", router);
+app.use("/auth", auth);
 
 // Start the server
 app.listen(port, () => {
